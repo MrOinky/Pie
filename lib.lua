56c86992eadb4f4b75e17c5d53d042e29297610a
MrOinky <61294271+MrOinky@users.noreply.github.com>
2025-10-06 18:58:42 +0100

    Change all `super:` to `super.`

--- lib.lua
+++ lib.lua
@@ -2,7 +2,7 @@ local Lib = {}
 
 function Lib:init()
     print("Thank you for using the Pie Library by Mr_Oinky!")
-    print("(Pie v1.2.1 for Kristal v0.8.0)")
+    print("(Pie v1.2.2 for Kristal v0.10.0-dev)")
 end
 
 return Lib
